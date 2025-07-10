8f87a358cba1417826f4061a89f6758e58b4b3a8
Leo Schultheiss <leo.schultheiss@fau.de>
2025-07-10 14:14:33 +0200

    integrate ammonite and trilobite plots

--- project.R
+++ project.R
@@ -228,8 +228,15 @@ barplot(sort(table(dat$diet), decreasing = TRUE),las=2,horiz = TRUE, )
 
 carnivores = dat[dat$diet == 'carnivore',]
 nonCarnivores = dat[dat$diet != 'carnivore',]
+
 herbivores = dat[dat$diet == 'herbivore' | dat$diet =='grazer', ]
 other = dat[dat$diet != 'carnivore' & dat$diet != 'herbivore' & dat$diet != 'grazer',]
+
+carnivores_ammoniteEx = dat[dat$diet == 'carnivore' & dat$order != "Ammonitida",]
+nonCarnivores_ammoniteEx = dat[dat$diet != 'carnivore' | dat$order == "Ammonitida",]
+
+carnivores_trilobiteEx = dat[dat$diet == 'carnivore' & dat$class != "Trilobita",]
+nonCarnivores_trilobiteEx = dat[dat$diet != 'carnivore' | dat$class == "Trilobita",]
 
 
 # what phyla do the samples belong to?
@@ -237,7 +244,7 @@ par(mar=c(4,11,4,4))
 barplot(sort(table(carnivores$phylum), decreasing = TRUE),las=2,horiz = TRUE)
 barplot(sort(table(herbivores$phylum), decreasing = TRUE),las=2,horiz = TRUE)
 barplot(sort(table(other$phylum), decreasing = TRUE),las=2,horiz = TRUE)
-par()
+par(mar=c(4,4,4,4))
 
 
 # analyze diversity dynamics of omnivores and others
@@ -247,18 +254,24 @@ divStg = function(x) {
 
 allDiv = divStg(dat)
 carniDiv = divStg(carnivores)
-predatorDiv = carniDiv
 nonCarniDiv = divStg(nonCarnivores)
-#herbiDiv = divStg(herbivores)
-#otherDiv = divStg(other)
 
-full_tsplot <- function(x=NULL, ys=NULL, cols=c("red", "black"), labels=c("Carnivores", "other"),
+carniAmmonExDiv = divStg(carnivores_ammoniteEx)
+noncarniAmmonExDiv = divStg(nonCarnivores_ammoniteEx)
+
+carniTriloExDiv = divStg(carnivores_trilobiteEx)
+noncarniTriloExDiv = divStg(nonCarnivores_trilobiteEx)
+
+carniCol = "brown1"
+nonCarniCol = "black"
+
+full_tsplot <- function(x=NULL, ys=NULL, cols=c(carniCol, nonCarniCol), labels=c("Carnivores", "Other"),
                            xlim = 4:95, ylim = c(0, 1),
                            ylab = "Diversity Proportion",
                            boxes = "sys", shading = "sys",
                            event_lines = c(66, 201, 252, 372, 445),
                           leg = TRUE,
-                           legend_pos = "topleft", legend_inset = c(0.05, 0.01),
+                           legend_pos = "topleft", legend_inset = c(0.05, 0.05),
                            legend_cex = 1.3,
                            tit="Diversity") {
   # Plot the tsplot background
@@ -290,40 +303,84 @@ full_tsplot <- function(x=NULL, ys=NULL, cols=c("red", "black"), labels=c("Carni
 
 
 # diversity rates
-full_tsplot(x=allDiv$mid, ys=list(carniDiv$divSIB, otherDiv$divSIB), 
+full_tsplot(x=allDiv$mid, ys=list(carniDiv$divSIB, nonCarniDiv$divSIB), 
             tit="Diversity",
             ylab="SIB Diversity",
-            ylim=c(0, max(carniDiv$divSIB, otherDiv$divSIB, na.rm = TRUE)))
+            ylim=c(0, max(carniDiv$divSIB, nonCarniDiv$divSIB, na.rm = TRUE)))
 
 # Total SIB diversity compared with carni diversity
+prop_tsplot = function(proportion, tit="Relative Makeup of Diets") {
+  full_tsplot(leg = FALSE, tit=tit, ylab="SIB proportion")
+  y_poly = c(proportion, rep(0, length(proportion)))
+  x = c(allDiv$mid, rev(allDiv$mid))
+  polygon(x, y_poly, col = rgb(1, 0, 0, 0.7), border = NA)
+  
+  y_poly_inv = c(proportion, rep(1, length(proportion)))
+  polygon(x, y_poly_inv, col=rgb(0, 0, 0, 0.7), border = NA)
+  abline(v=c(66, 201, 252, 372, 445), col="white")
+  legend("topleft", bg = "white", legend = c("Carnivores", "other"), title="Diet",
+         col = c(rgb(1, 0, 0), rgb(0, 0, 0)), lwd = 2, inset = c(0.05, 0.05),
+         cex = 1.3,)
+  
+}
 total = allDiv$divSIB
 carni = carniDiv$divSIB
 proportion = carni / total
 
-full_tsplot(leg = FALSE, tit="Relative Makeup of Diets", ylab="SIB proportion")
-y_poly = c(proportion, rep(0, length(proportion)))
-x = c(allDiv$mid, rev(allDiv$mid))
-polygon(x, y_poly, col = rgb(1, 0, 0, 0.7), border = NA)
+prop_tsplot(proportion)
 
-y_poly_inv = c(proportion, rep(1, length(proportion)))
-polygon(x, y_poly_inv, col=rgb(0, 0, 0, 0.7), border = NA)
-abline(v=c(66, 201, 252, 372, 445), col="white")
-legend("topleft", bg = "white", legend = c("Carnivores", "other"), title="Diet",
-       col = c(rgb(1, 0, 0), rgb(0, 0, 0)), lwd = 2, inset = c(0.05, 0.01),
-       cex = 1.3,)
 
+# exclude ammonites
+total = allDiv$divSIB
+carni = carniAmmonExDiv$divSIB
+proportion = carni / total
+prop_tsplot(proportion,tit="Relative Makeup of Diets (Ammonites Counted towards Other)")
+
+# exclude trilobites
+total = allDiv$divSIB
+carni = carniTriloExDiv$divSIB
+proportion = carni / total
+prop_tsplot(proportion, tit="Relative Makeup of Diets (Trilobites Counted towards Other)")
+
+
+##### total extinction rates
+full_tsplot(x=allDiv$mid, ys=list(carniDiv$extPC, nonCarniDiv$extPC), 
+            tit="Extinctions",
+            ylab="Foote Metric",
+            ylim=c(0, max(max(carniAmmonExDiv$extPC, na.rm=TRUE), 
+                          max(noncarniAmmonExDiv$extPC, na.rm=TRUE),
+                          max(carniTriloExDiv$extPC, na.rm=TRUE),
+                          max(noncarniTriloExDiv$extPC, na.rm=TRUE)))
+            )
+# add ammonite lines
+lines(x=allDiv$mid, y=carniAmmonExDiv$extPC, lwd = 2, col = carniCol, type = "l", lty=2)
+lines(x=allDiv$mid, y=noncarniAmmonExDiv$extPC, lwd = 2, col = nonCarniCol, type = "l", lty=2)
+legend("topright", bg = "white", legend = c("Default", "Ammonites not carnivore"), lwd = 2, lty=c(1, 2),inset = c(0.05, 0.05), cex = 1.3)
+
+# add trilobite lines
+lines(x=allDiv$mid, y=carniTriloExDiv$extPC, lwd = 2, col = carniCol, type = "l", lty=3)
+lines(x=allDiv$mid, y=noncarniTriloExDiv$extPC, lwd = 2, col = nonCarniCol, type = "l", lty=3)
+legend("topright", bg = "white", legend = c("Default", "Ammonites not carnivore", "Trilobites not carnivore"), lwd = 2, lty=c(1, 2, 3),inset = c(0.05, 0.05), cex = 1.3)
+
+
+
+
+# total corrected
+full_tsplot(x=allDiv$mid, ys=list(carniDiv$ext2f3, nonCarniDiv$ext2f3), 
+            tit="Extinctions",
+            ylab="Foote Metric", ylim = c(0,2))
 
 # relative extinction rates
-#full_tsplot(x=allDiv$mid, ys=list(carniDiv$extProp, herbiDiv$extProp, otherDiv$extProp), 
-#            tit="Proportional Extinctions",
-#            ylab="Proportion")
 full_tsplot(x=allDiv$mid, ys=list(carniDiv$extProp, nonCarniDiv$extProp), 
-                        tit="Extinctions",
-                        ylab="Proportion")
+                        tit="Extinctions",ylab="Proportion")
+full_tsplot(x=allDiv$mid, ys=list(carniAmmonExDiv$extProp, noncarniAmmonExDiv$extProp), 
+            tit="Extinctions (ammonites counted as non-carnivores)",
+            ylab="Proportion")
+full_tsplot(x=allDiv$mid, ys=list(carniTriloExDiv$extProp, noncarniTriloExDiv$extProp), 
+            tit="Extinctions (ammonites counted as non-carnivores)",
+            ylab="Proportion")
 
 
-# investigate carnivore spike early triassic
-carniTriassic = carniDiv[carniDiv$system == "Triassic",]
 
 # zscored extinction rates (raw data, foote metric, since biases don't matter for this analysis)
 zscore = function(x) {  return (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)  }
@@ -331,8 +388,41 @@ carniExtZscore = zscore(carniDiv$extPC)
 nonCarniExtZscore = zscore(nonCarniDiv$extPC)
 
 # Compare Z-scored values
-full_tsplot(allDiv$mid, ys=list(carniExtZscore, nonCarniExtZscore), ylim=c(-2, 2), tit="Extinction Rates",ylab="Foote Metric (Z-Scored)", cols=c("red", "black"), labels=c("Carnivores", "Other"))
+full_tsplot(allDiv$mid, ys=list(carniExtZscore, nonCarniExtZscore), ylim=c(-0.5, 1.5), tit="Extinction Rates",ylab="Foote Metric (Z-Scored)")
 
 # difference between zscores
 carniExtDiffZscore = carniExtZscore - nonCarniExtZscore
-full_tsplot(allDiv$mid, ys=list(carniExtDiffZscore), ylim=c(-1,1), tit="Difference between Carnivore Extinctions and others", cols=c("red"), labels=c("Predators - Non-Predators"))
+full_tsplot(allDiv$mid, ys=list(carniExtDiffZscore), ylim=c(-0.5,1.5), tit="Difference between Carnivore Extinctions and others",ylab="Foote Metric (Z-Scored)",  cols=c("brown1"), labels=c("Carnivores - Non-Carnivores"))
+
+
+### mass extinctions
+logOdds = function(a, b) {
+  log(sum(a, na.rm = TRUE) / sum(b, na.rm=TRUE))
+}
+
+extinctions = c(20, 34, 51, 58, 81)
+non_extinctions = 0:94
+non_extinctions = non_extinctions[-extinctions] + 1
+
+logOdds(carniDiv$extPC[extinctions], nonCarniDiv$extPC[extinctions])
+logOdds(carniDiv$extPC[non_extinctions], nonCarniDiv$extPC[non_extinctions])
+
+logOdds(carniDiv$ext2f3[extinctions], nonCarniDiv$ext2f3[extinctions])
+logOdds(carniDiv$ext2f3[non_extinctions], nonCarniDiv$ext2f3[non_extinctions])
+
+extintions_nonperm = c(20, 34, 58, 81)
+
+logOdds(carniDiv$ext2f3[extintions_nonperm], nonCarniDiv$ext2f3[extintions_nonperm])
+
+
+
+boxplot(carniDiv$ext2f3, nonCarniDiv$ext2f3, cols =c(carniCol, nonCarniCol), xlab="Diet", ylab="Foote Extinction Rate")
+points(rep(1, 5), carniDiv$extPC[extinctions], col="red",pch=21, bg="white", cex=1.)
+points(rep(2, 5), nonCarniDiv$extPC[extinctions], col="red",pch=21, bg="white", cex=1.)
+legend("topright", legend=c("Mass Extinctions"), col=c("red"), cex=1.3, inset=c(0.05,0.05), pch=21)
+
+
+wilcox.test(carniDiv$extPC, nonCarniDiv$extPC, alternative = "two.sided")
+
+boxplot(carniAmmonExDiv$extPC, noncarniAmmonExDiv$extPC, cols =c(carniCol, nonCarniCol))
+wilcox.test(carniAmmonExDiv$extPC, noncarniAmmonExDiv$extPC, alternative = "two.sided")
